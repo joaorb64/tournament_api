ec22cc61fc1bfd43597c0b6938afcb4f4a25a7a1
João Ribeiro Bezerra <joaorb64@gmail.com>
2021-03-11 21:13:13 -0300

    Fix smashgg

--- get_smashgg_user_data.py
+++ get_smashgg_user_data.py
@@ -101,8 +101,8 @@ def fetchPlayerDo(currKey, playerIndex):
 
 
 	if previous_cache.get(str(player["smashgg_id"]), None) is not None and "latestSetId" in previous_cache[str(player["smashgg_id"])]:
-		if resp.get("player", {}).get("sets", None) is not None:
-			if len(resp["player"]["sets"]["nodes"]) > 0 and resp["player"]["sets"]["nodes"][0] is not None:
+		if resp.get("player", {}).get("sets", {}).get("nodes", None) != None:
+			if resp["player"]["sets"]["nodes"][0] is not None:
 				if resp["player"]["sets"]["nodes"][0]["id"] == previous_cache[str(player["smashgg_id"])]["latestSetId"]:
 					newSets = False
 					if "character_usage" in previous_cache[str(player["smashgg_id"])]:
