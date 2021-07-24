f021e16e600aa65b84ba0dd276ebccb65bd5054d
João Ribeiro Bezerra <joaorb64@gmail.com>
2021-07-24 12:15:49 -0300

    Change next tournaments logic

--- get_next_tournaments.py
+++ get_next_tournaments.py
@@ -15,6 +15,12 @@ if os.path.exists("auth.json"):
 else:
   SMASHGG_KEYS = os.environ.get("SMASHGG_KEYS")
 
+f = open('./smashgg_countrycodes.json')
+smashgg_countrycodes = json.load(f)
+
+f = open('./countries+states+cities.json')
+countries_json = json.load(f)
+
 def get_next_tournaments(game):
     currentKey = 0
 
@@ -25,41 +31,41 @@ def get_next_tournaments(game):
     countries = json.load(f)
     
     oldTournaments = {}
+    updateTime = 0
 
     try:
         f = open('./out/'+game+'/nexttournaments.json')
         oldTournaments = json.load(f)
+        updateTime = oldTournaments.get("updateTime", 0)
     except Exception as e:
         print("No previous tournaments file")
 
-    for country in countries:
+    page = 1
 
-        page = 1
+    tournaments = []
 
-        tournaments = []
-
-        while True:
-            r = requests.post(
-                'https://api.smash.gg/gql/alpha',
-                headers={
-                    'Authorization': 'Bearer'+SMASHGG_KEYS[currentKey],
-                },
-                json={
-                    'query': '''
-                    query TournamentsByCountry($cCode: String!, $perPage: Int!) {
-                        tournaments(query: {
+    while True:
+        r = requests.post(
+            'https://api.smash.gg/gql/alpha',
+            headers={
+                'Authorization': 'Bearer'+SMASHGG_KEYS[currentKey],
+            },
+            json={
+                'query': '''
+                query Tournaments($perPage: Int!) {
+                    tournaments(query: {
                         perPage: $perPage
                         page: '''+str(page)+'''
                         filter: {
-                            countryCode: $cCode
-                            videogameIds: ['''+str(config["smashgg_videogame_id"])+''']
-                            upcoming: true
+                            videogameIds: ['''+str(config["smashgg_videogame_id"])+'''],
+                            upcoming: true,
+                            computedUpdatedAt: '''+str(int(updateTime))+'''
                         }
-                        }) {
-                        nodes {
-                            id
-                            startAt
-                            events {
+                    }) {
+                    nodes {
+                        id
+                        startAt
+                        events {
                             id
                             videogame {
                                 id
@@ -67,43 +73,42 @@ def get_next_tournaments(game):
                             startAt
                             }
                         }
-                        }
-                    },
-                    ''',
-                    'variables': {
-                    "cCode": country,
+                    }
+                },
+                ''',
+                'variables': {
                     "perPage": 20
-                    },
-                }
-            )
-            time.sleep(4/len(SMASHGG_KEYS))
-            currentKey = (currentKey+1)%len(SMASHGG_KEYS)
+                },
+            }
+        )
+        time.sleep(4/len(SMASHGG_KEYS))
+        currentKey = (currentKey+1)%len(SMASHGG_KEYS)
 
-            resp = json.loads(r.text)
+        resp = json.loads(r.text)
 
-            if resp is None or \
-            resp.get("data") is None or \
-            resp["data"].get("tournaments") is None or \
-            resp["data"]["tournaments"].get("nodes") is None:
-                print(country+" "+str(resp))
-                break
-        
-            data = resp["data"]["tournaments"]["nodes"]
+        if resp is None or \
+        resp.get("data") is None or \
+        resp["data"].get("tournaments") is None or \
+        resp["data"]["tournaments"].get("nodes") is None:
+            print(str(resp))
+            break
+    
+        data = resp["data"]["tournaments"]["nodes"]
 
-            if data == None or len(data) == 0:
-                break
+        if data == None or len(data) == 0:
+            break
 
-            print(country+" - Page: "+str(page)+"\tTournaments: "+str(len(data)))
+        print("Page: "+str(page)+"\tTournaments: "+str(len(data)))
 
-            for tournament in data:
-                r = requests.post(
-                    'https://api.smash.gg/gql/alpha',
-                    headers={
-                    'Authorization': 'Bearer'+SMASHGG_KEYS[currentKey],
-                    },
-                    json={
-                    'query': '''
-                        query Tournament($tournamentId: ID!) {
+        for tournament in data:
+            r = requests.post(
+                'https://api.smash.gg/gql/alpha',
+                headers={
+                'Authorization': 'Bearer'+SMASHGG_KEYS[currentKey],
+                },
+                json={
+                'query': '''
+                    query Tournament($tournamentId: ID!) {
                         tournament(id: $tournamentId) {
                             id
                             name
@@ -117,93 +122,148 @@ def get_next_tournaments(game):
                             venueAddress
                             addrState
                             events {
-                            id
-                            name
-                            isOnline
-                            state
-                            numEntrants
-                            videogame {
-                                id
-                            }
-                            startAt
-                            phaseGroups {
-                                id
-                                phase {
                                 id
                                 name
+                                slug
+                                isOnline
+                                state
+                                numEntrants
+                                videogame {
+                                    id
                                 }
-                                progressionsOut {
-                                id
+                                startAt
+                                phaseGroups {
+                                    id
+                                    phase {
+                                        id
+                                        name
+                                    }
+                                    progressionsOut {
+                                        id
+                                    }
                                 }
-                            }
                             }
                             streams {
-                            streamName
+                                streamName
                             }
                             images{
-                            id
-                            url
-                            type
+                                id
+                                url
+                                type
                             }
+                            countryCode
                         }
-                        },
-                    ''',
-                    'variables': {
-                        "tournamentId": tournament["id"]
                     },
-                    }
-                )
-                time.sleep(1/len(SMASHGG_KEYS))
-                currentKey = (currentKey+1)%len(SMASHGG_KEYS)
+                ''',
+                'variables': {
+                    "tournamentId": tournament["id"]
+                },
+                }
+            )
+            time.sleep(1/len(SMASHGG_KEYS))
+            currentKey = (currentKey+1)%len(SMASHGG_KEYS)
 
-                resp = json.loads(r.text)
-                tournament_data = resp["data"]["tournament"]
+            resp = json.loads(r.text)
+            tournament_data = resp.get("data", {}).get("tournament")
 
-                smash_ultimate_tournaments = 0
+            smash_ultimate_tournaments = 0
 
-                if tournament_data == None or tournament_data["events"] == None:
+            if tournament_data == None or tournament_data["events"] == None:
+                continue
+
+            for event in tournament_data["events"]:
+                if event["videogame"]["id"] == config["smashgg_videogame_id"]:
+                    smash_ultimate_tournaments += 1
+
+            for event in tournament_data["events"]:
+                # Smash Ultimate
+                if event["videogame"]["id"] != config["smashgg_videogame_id"]:
                     continue
 
-                for event in tournament_data["events"]:
-                    if event["videogame"]["id"] == config["smashgg_videogame_id"]:
-                        smash_ultimate_tournaments += 1
+                if event["startAt"] > time.time():
+                    event["tournament"] = tournament_data["name"]
+                    event["tournament_id"] = tournament_data["id"]
+                    event["city"] = tournament_data["city"]
+                    event["url"] = "https://smash.gg"+tournament_data["url"]+"/"+event["slug"]
+                    event["streams"] = tournament_data["streams"]
+                    event["timezone"] = tournament_data["timezone"]
+                    event["tournament_startAt"] = tournament_data["startAt"]
+                    event["tournament_endAt"] = tournament_data["endAt"]
+                    event["tournament_registrationClosesAt"] = tournament_data["registrationClosesAt"]
+                    event["images"] = tournament_data["images"]
+                    event["tournament_multievent"] = False if smash_ultimate_tournaments <= 1 else True
+                    event["tournament_venueName"] = tournament_data["venueName"]
+                    event["tournament_venueAddress"] = tournament_data["venueAddress"]
+                    event["tournament_addrState"] = tournament_data["addrState"]
+                    event["country_code"] = tournament_data["countryCode"]
 
-                for event in tournament_data["events"]:
-                    # Smash Ultimate
-                    if event["videogame"]["id"] != config["smashgg_videogame_id"]:
-                        continue
+                    r = requests.get("https://api.smash.gg"+tournament_data["url"])
+                    oldApiData = json.loads(r.text)
 
-                    if event["startAt"] > time.time():
-                        event["tournament"] = tournament_data["name"]
-                        event["tournament_id"] = tournament_data["id"]
-                        event["city"] = tournament_data["city"]
-                        event["url"] = "https://smash.gg"+tournament_data["url"]
-                        event["streams"] = tournament_data["streams"]
-                        event["timezone"] = tournament_data["timezone"]
-                        event["tournament_startAt"] = tournament_data["startAt"]
-                        event["tournament_endAt"] = tournament_data["endAt"]
-                        event["tournament_registrationClosesAt"] = tournament_data["registrationClosesAt"]
-                        event["images"] = tournament_data["images"]
-                        event["tournament_multievent"] = False if smash_ultimate_tournaments <= 1 else True
-                        event["tournament_venueName"] = tournament_data["venueName"]
-                        event["tournament_venueAddress"] = tournament_data["venueAddress"]
-                        event["tournament_addrState"] = tournament_data["addrState"]
+                    attendeeRequirements = \
+                        oldApiData.get("entities", {})\
+                        .get("tournament", {})\
+                        .get("attendeeRequirements", {})
                     
-                    tournaments.append(event)
+                    countryRequirements = None
+                    if attendeeRequirements != None:
+                        countryRequirements = attendeeRequirements.get("country", None)
 
-            page+=1
-        
-        for oldTournament in oldTournaments.get(country, {}).get("events", []):
-            if "tournament_endAt" in oldTournament and "id" in oldTournament:
-                found = next((t for t in tournaments if t.get("id", None) == oldTournament["id"]), None)
+                    if countryRequirements:
+                        regionLock = []
+                        for country in countryRequirements:
+                            name = next((c["country"] for c in smashgg_countrycodes if c["id"] == country), None)
+                            if name:
+                                ccode = next((c["iso2"] for c in countries_json if c["name"]==name or c["native"]==name), None)
+                                if ccode:
+                                    name = ccode
+                                codemap = {
+                                    "Croatia": "HR",
+                                    "Netherlands": "NL",
+                                    "England": "GB",
+                                    "Wales": "GB",
+                                    "Scotland": "GB",
+                                    "Åland Islands": "AX",
+                                    "United States Virgin Islands": "VI",
+                                    "Vatican City": "VA",
+                                    "Saint Vincent and the Grenadines": "VC",
+                                    "Trinidad and Tobago": "TT",
+                                    "Turks and Caicos Islands": "TC",
+                                    "Sint Maarten": "SX",
+                                    "Antigua and Barbuda": "AG",
+                                    "Bonaire": "BQ",
+                                    "Bahamas": "BS",
+                                    "Saint Kitts and Nevis": "KN",
+                                    "Saint Martin": "MF",
+                                    "British Virgin Islands": "VG",
+                                    "South Korea": "KR",
+                                    "Hong Kong": "HK"
+                                }
+                                if name in codemap:
+                                    name = codemap[name]
+                                
+                                regionLock.append(name)
+                        event["region_lock"] = regionLock
+                        print(regionLock)
+                
+                tournaments.append(event)
 
-                if not found and time.time() <= oldTournament["tournament_endAt"]:
-                    tournaments.append(oldTournament)
+        print("Tournament number: "+str(len(tournaments)))
+        page+=1
+    
+    for oldTournament in oldTournaments.get("events", []):
+        if "tournament_endAt" in oldTournament and "id" in oldTournament:
+            found = next((t for t in tournaments if t.get("id", None) == oldTournament["id"]), None)
 
-        countries[country]["events"] = tournaments
+            if not found and time.time() <= oldTournament["tournament_endAt"]:
+                tournaments.append(oldTournament)
 
     with open('./out/'+game+'/nexttournaments.json', 'w') as outfile:
-        json.dump(countries, outfile, indent=4)
+        json.dump(
+            {"updateTime": int(time.time()), "events": tournaments},
+            outfile,
+            indent=4
+        )
 
 if __name__ == "__main__":
     games = os.listdir("./games")
